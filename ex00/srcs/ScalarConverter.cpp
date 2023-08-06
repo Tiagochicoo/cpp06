71356144db9681110ae5052fad49dff41aadb7e9
tpereira <tpereira@student.42lisboa.com>
2023-08-06 15:45:25 +0100

    handling edge cases

--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 00:10:18 by tpereira          #+#    #+#             */
-/*   Updated: 2023/08/06 15:14:39 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/08/06 15:44:24 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,8 @@ void ScalarConverter::getType(std::string &str)
 	}
 	else if (str == "nan" || str == "nanf")
 		setType(PSEUDO);
+	else if (str == "-nan" || str == "-nanf")
+		setType(NOT);
 	else if (str == "inf" || str == "inff")
 		setType(PSEUDO);
 	else if (str == "-inf" || str == "-inff")
@@ -123,26 +125,18 @@ int ScalarConverter::calculatePrecision(std::string& str)
 void ScalarConverter::getValue(std::string &str)
 {
 	if (_type == CHAR)
-	{
 		setValue(static_cast<double>(str[0]));
-	}
 	else if (_type == INT || _type == DOUBLE)
 	{
 		std::istringstream stream(_str);
 		stream >> _value;
 	}
 	else if (_type == FLOAT)
-	{
 		setValue(static_cast<double>(atof(str.c_str())));
-	}
 	else if (_type == PSEUDO)
-	{
 		setValue(static_cast<double>(strtod(str.c_str(), NULL)));
-	}
 	else
-	{
 		setValue(0);
-	}
 }
 
 void ScalarConverter::convert(std::string &str)
@@ -204,27 +198,12 @@ void ScalarConverter::printFloat()
 
 void ScalarConverter::printDouble()
 {
-    switch (_type)
-    {
-    case CHAR:
-        std::cout << "double: " << _value << std::endl;
-        break;
-    case INT:
-        std::cout << "double: " << _value << ".0" << std::endl;
-        break;
-    case FLOAT:
-        std::cout << "double: " << _value << std::endl;
-        break;
-    case DOUBLE:
-        std::cout << "double: " << _value << std::endl;
-        break;
-    case PSEUDO:
-        std::cout << "double: " << _value << std::endl;
-        break;
-    default:
-        std::cout << "double: impossible" << std::endl;
-        break;
-    }
+	if (_type == CHAR || _type == INT)
+		std::cout << "double: " << _value << ".0" << std::endl;
+	else if (_type == FLOAT || _type == DOUBLE || _type == PSEUDO)
+		std::cout << "double: " << _value << std::endl;
+	else
+		std::cout << "double: impossible" << std::endl;
 }
 
 
