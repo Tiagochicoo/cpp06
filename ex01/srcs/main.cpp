cfdb7e93302163a590ef545809b4d9f81475fe2b
Tiago Pereira <tpereira@c2r11s6.42lisboa.com>
2023-08-11 19:36:48 +0100

    added main.cpp tests

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,35 +6,51 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 18:32:05 by tpereira          #+#    #+#             */
-/*   Updated: 2023/08/11 19:24:09 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/08/11 19:36:29 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <iostream>
+#include <iomanip>
 #include "Serializer.hpp"
 #include "Data.hpp"
 
 int main(void)
 {
-    Data *ptr = new Data();
-    Serializer seri;
-    uintptr_t num;
+	Data *ptr = new Data();
+	Serializer seri;
+	uintptr_t num;
 
-    ptr->name = "Cast";
-    ptr->value = 42;
-    std::cout << "Original Data *ptr:\n";
-    std::cout << "ptr->name: " << ptr->name << std::endl;
-    std::cout << "ptr->value: " << ptr->value << std::endl;
-    num = seri.serialize(ptr);
+	ptr->name = "test";
+	ptr->value = 42;
+	num = 1;
+
+	std::cout << "Original Data *ptr:\n";
+	std::cout << "ptr->name: " << ptr->name << std::endl;
+	std::cout << "ptr->value: " << ptr->value << std::endl;
+	std::cout << "num: " << num << std::endl;
+
+	num = seri.serialize(ptr);
 	std::cout << "\nSerialized Data *ptr:\n";
-    std::cout << "ptr->name: " << ptr->name << std::endl;
-    std::cout << "ptr->value: " << ptr->value << std::endl;
+	std::cout << "ptr->name: " << ptr->name << std::endl;
+	std::cout << "ptr->value: " << ptr->value << std::endl;
+	std::cout << "num: " << num << std::endl;
+	
 	ptr = seri.deserialize(num);
 	std::cout << "\nDeserialized Data *ptr:\n";
-    std::cout << "ptr->name: " << ptr->name << std::endl;
-    std::cout << "ptr->value: " << ptr->value << std::endl;
+	std::cout << "ptr->name: " << ptr->name << std::endl;
+	std::cout << "ptr->value: " << ptr->value << std::endl;
+	std::cout << "num: " << num << std::endl;
 
-    delete ptr;
-    return 0;
+	ptr->name = "usable!";
+	ptr->value = 0;
+	num = 999;
+	std::cout << "\nUsable Data *ptr:\n";
+	std::cout << "ptr->name: " << ptr->name << std::endl;
+	std::cout << "ptr->value: " << ptr->value << std::endl;
+	std::cout << "num: " << num << std::endl;
+
+	delete ptr;
+	return 0;
 }
