b10428f6b24db390dc33511c7620ae5345c4d718
Tiago Pereira <tpereira@c2r11s6.42lisboa.com>
2023-08-11 19:24:41 +0100

    ex01 seems to be done

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 18:32:05 by tpereira          #+#    #+#             */
-/*   Updated: 2023/08/11 18:59:32 by tpereira         ###   ########.fr       */
+/*   Updated: 2023/08/11 19:24:09 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,23 @@
 
 int main(void)
 {
-    Data *ptr = new Data;
+    Data *ptr = new Data();
     Serializer seri;
     uintptr_t num;
 
-    ptr->name = "test";
+    ptr->name = "Cast";
     ptr->value = 42;
-    std::cout << "Data *ptr:\n\n";
-    std::cout << "ptr->name " << ptr->name << std::endl;
-    std::cout << "ptr->value " << ptr->value << std::endl;
+    std::cout << "Original Data *ptr:\n";
+    std::cout << "ptr->name: " << ptr->name << std::endl;
+    std::cout << "ptr->value: " << ptr->value << std::endl;
     num = seri.serialize(ptr);
-    std::cout << num << std::endl;
-    std::cout << ptr << std::endl;
+	std::cout << "\nSerialized Data *ptr:\n";
+    std::cout << "ptr->name: " << ptr->name << std::endl;
+    std::cout << "ptr->value: " << ptr->value << std::endl;
+	ptr = seri.deserialize(num);
+	std::cout << "\nDeserialized Data *ptr:\n";
+    std::cout << "ptr->name: " << ptr->name << std::endl;
+    std::cout << "ptr->value: " << ptr->value << std::endl;
 
     delete ptr;
     return 0;
